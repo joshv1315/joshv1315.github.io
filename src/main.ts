e8479b222a16c479afae16aefe421a27c9bee151
Joshua Vijayasegar <joshuavijay@Joshuas-MacBook-Air.local>
2026-05-05 00:20:02 -0400

    changed wording again

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ const personalProjects: Project[] = [
   },
   {
     name: "hillklimb.com",
-    description: "Job search platform optimized for new grad SWE/PM recruiting.",
+    description: "Job search platform optimized for early career SWE/PM recruiting.",
     url: "#",
     tags: ["In Development"],
   },
